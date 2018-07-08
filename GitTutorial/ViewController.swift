306f07c6cfdb452f85be80875df1d60c51a8ff4f
Thomas Czerniawski <thomas.czerniawski@gmail.com>
2018-07-07 20:16:26 -0500

    reverted print commands

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("hello world!")
-        print("CS193P")
         super.viewDidLoad()
     }
 
